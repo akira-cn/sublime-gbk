1fad05b00dc78d22b7fe954ade2bd80cec30a6c6
Wu Akira <akira@Wu-AkiramatoMacBook-Air.local>
2012-02-24 11:18:15 +0800

    bugfixed - chinese file name will crash st

--- sublime_gbk.py
+++ sublime_gbk.py
@@ -16,22 +16,24 @@ def gbk2utf8(view):
 		text = gbk.decode('gbk')
 		
 		file_name = view.file_name().encode('utf-8')
-		print urllib.quote_plus(file_name)
 
-		tmp_file_name = os.path.basename(file_name)  + SEPERATOR + urllib.quote_plus(file_name)
+		tmp_file_name = urllib.quote_plus(os.path.basename(file_name))  + SEPERATOR + urllib.quote_plus(file_name)
 		tmp_file = os.path.join(TEMP_PATH, tmp_file_name)
 
-		file(tmp_file, 'w').write(text.encode('utf8'))
+		f = file(tmp_file, 'w')
+		f.write(text.encode('utf8'))
+		f.close()
 
 		window = sublime.active_window()
-		window.run_command('close')
-
+		
 		v = window.find_open_file(tmp_file)
 
 		if(not v):
 			window.open_file(tmp_file)
-		else:
-			window.focus_view(v)
+
+		window.focus_view(view)
+		window.run_command('close')
+		window.focus_view(v)
 
 		sublime.status_message('gbk encoding detected, open with utf8.')
 
@@ -57,8 +59,13 @@ class SaveWithGbkCommand(sublime_plugin.TextCommand):
 	def __init__(self, view):
 		self.view = view
 	def run(self, edit):
-		parts = self.view.file_name().split(SEPERATOR)
-		if(not self.view.file_name().startswith(TEMP_PATH) and len(parts) <= 1):
+		file_name = self.view.file_name()
+
+		if(not file_name):
+			return
+
+		parts = file_name.split(SEPERATOR)
+		if(not file_name.startswith(TEMP_PATH) and len(parts) <= 1):
 			saveWithEncoding(self.view)
 			sublime.active_window().run_command('close')
 			sublime.active_window().open_file(self.view.file_name())
@@ -69,9 +76,14 @@ class SaveWithUtf8Command(sublime_plugin.TextCommand):
 	def __init__(self, view):
 		self.view = view
 	def run(self, edit):
-		parts = self.view.file_name().split(SEPERATOR)
+		file_name = self.view.file_name()
 
-		if(self.view.file_name().startswith(TEMP_PATH) and len(parts) > 1):
+		if(not file_name):
+			return
+
+		parts = file_name.split(SEPERATOR)
+
+		if(file_name.startswith(TEMP_PATH) and len(parts) > 1):
 			file_name = urllib.unquote_plus(parts[1].encode('utf-8')).decode('utf-8')
 			saveWithEncoding(self.view, file_name, 'utf-8')
 			sublime.active_window().run_command('close')
