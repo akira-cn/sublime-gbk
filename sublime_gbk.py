853ad1dff1d9d30fc4e69ddc7262062d4fea47f9
jamiesun <jamiesun.net@gmail.com>
2012-07-02 00:03:50 +0800

    修改了临时文件的存储方式
    主要是修改了临时文件的命名方式，原来插件使用编码中文路径的方式在tab栏占用宽度过大，
    同时在新版本中不能显示中文文件名，感觉不是很好，因此对临时文件直接在文件所在目录
    建立一个dump文件，也就是后缀直接加上dump，在tab显示仅仅多了一个'.dump',对于中文路径
    采用unicode是可以正常显示的，不用编码。
    
    另外增加了一个关闭文件的事件监听，就是将dump文件删掉（在源文件已经保存的基础上），如果
    不删掉临时文件，日积月累，会产生很多垃圾文件。

--- sublime_gbk.py
+++ sublime_gbk.py
@@ -2,91 +2,84 @@
 
 import sublime, sublime_plugin
 import os, re
-import urllib
-
-TEMP_PATH = os.path.join(os.getcwd(), 'tmp')
-SEPERATOR = '                '
 
 def gbk2utf8(view):
-	try:
-		reg_all = sublime.Region(0, view.size())
-		gbk = view.substr(reg_all).encode('gbk')
-	except:
-		gbk = file(view.file_name()).read()
-		text = gbk.decode('gbk')
-		
-		file_name = view.file_name().encode('utf-8')
+    try:
+        reg_all = sublime.Region(0, view.size())
+        gbk = view.substr(reg_all).encode('gbk')
+    except:
+        gbk = file(view.file_name()).read()
+        text = gbk.decode('gbk')
 
-		tmp_file_name = urllib.quote_plus(os.path.basename(file_name))  + SEPERATOR + urllib.quote_plus(file_name)
-		tmp_file = os.path.join(TEMP_PATH, tmp_file_name)
+        tmp_file = u"%s.dump"%view.file_name()
+        f = file(tmp_file, 'w')
+        f.write(text.encode('utf8'))
+        f.close()
 
-		f = file(tmp_file, 'w')
-		f.write(text.encode('utf8'))
-		f.close()
+        window = sublime.active_window()
+        
+        tmp_view = window.open_file(tmp_file)
 
-		window = sublime.active_window()
-		
-		v = window.find_open_file(tmp_file)
-
-		if(not v):
-			window.open_file(tmp_file)
-
-		window.focus_view(view)
-		window.run_command('close')
-		window.focus_view(v)
-
-		sublime.status_message('gbk encoding detected, open with utf8.')
+        if not tmp_view:
+            window.open_file(tmp_file)
+        
+        window.focus_view(view)
+        window.run_command('close')
+        window.focus_view(tmp_view)
+        sublime.status_message('gbk encoding detected, open with utf8.')
 
 def saveWithEncoding(view, file_name = None, encoding = 'gbk'):
-	if(not file_name):
-		file_name = view.file_name()
-	reg_all = sublime.Region(0, view.size())
-	text = view.substr(reg_all).encode(encoding)
-	gbk = file(file_name, 'w')
-	gbk.write(text)
-	gbk.close()	
+    if not file_name:
+        file_name = view.file_name()
+    reg_all = sublime.Region(0, view.size())
+    text = view.substr(reg_all).encode(encoding)
+    gbk = file(file_name, 'w')
+    gbk.write(text)
+    gbk.close()    
 
 class EventListener(sublime_plugin.EventListener):
-	def on_load(self, view):
-		gbk2utf8(view)
-	def on_post_save(self, view):
-		parts = view.file_name().split(SEPERATOR)
-		if(view.file_name().startswith(TEMP_PATH) and len(parts) > 1):
-			file_name = urllib.unquote_plus(parts[1].encode('utf-8')).decode('utf-8')
-			saveWithEncoding(view, file_name)
+    def on_load(self, view):
+        gbk2utf8(view)
+
+    def on_post_save(self, view):
+        if ".dump" in view.file_name():
+            file_name = view.file_name()[:-5]
+            saveWithEncoding(view, file_name)
+
+    def on_close(self,view):
+        if ".dump" in view.file_name():
+            os.remove(view.file_name())
+
 
 class SaveWithGbkCommand(sublime_plugin.TextCommand):
-	def __init__(self, view):
-		self.view = view
-	def run(self, edit):
-		file_name = self.view.file_name()
+    def __init__(self, view):
+        self.view = view
+    def run(self, edit):
+        file_name = self.view.file_name()
 
-		if(not file_name):
-			return
+        if(not file_name):
+            return
 
-		parts = file_name.split(SEPERATOR)
-		if(not file_name.startswith(TEMP_PATH) and len(parts) <= 1):
-			saveWithEncoding(self.view)
-			sublime.active_window().run_command('close')
-			sublime.active_window().open_file(self.view.file_name())
-		else:
-			sublime.active_window().run_command('save')
+        if ".dump" not in self.view.file_name():
+            saveWithEncoding(self.view)
+            sublime.active_window().run_command('close')
+            sublime.active_window().open_file(self.view.file_name())
+        else:
+            sublime.active_window().run_command('save')
 
 class SaveWithUtf8Command(sublime_plugin.TextCommand):
-	def __init__(self, view):
-		self.view = view
-	def run(self, edit):
-		file_name = self.view.file_name()
+    def __init__(self, view):
+        self.view = view
+    def run(self, edit):
+        file_name = self.view.file_name()
 
-		if(not file_name):
-			return
+        if(not file_name):
+            return
 
-		parts = file_name.split(SEPERATOR)
-
-		if(file_name.startswith(TEMP_PATH) and len(parts) > 1):
-			file_name = urllib.unquote_plus(parts[1].encode('utf-8')).decode('utf-8')
-			saveWithEncoding(self.view, file_name, 'utf-8')
-			sublime.active_window().run_command('close')
-			sublime.active_window().open_file(file_name)
-		else:
-			sublime.active_window().run_command('save')
+        if ".dump" in self.view.file_name():
+            file_name = self.view.file_name()[:-5]
+            saveWithEncoding(self.view, file_name, 'utf-8')
+            sublime.active_window().run_command('close')
+            sublime.active_window().open_file(file_name)
+        else:
+            sublime.active_window().run_command('save')
